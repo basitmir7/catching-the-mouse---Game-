be6d311dab2dc8b35b3da0da9603f4519dd49b9b
Basit Mir <80674953+basitmir7@users.noreply.github.com>
2022-05-13 19:29:20 +0530

    added the level functionality

--- app.js
+++ app.js
@@ -5,19 +5,48 @@ const closeBtn = document.getElementById("close-modal-btn");
 const container = document.getElementById("container");
 const ModalTime = document.getElementById("modal-time");
 const gameBody = document.getElementById("gameBody");
-// var cancelled = false;
-//function to mov the mouse from randomly
+var easy = document.getElementById("easy");
+var medium = document.getElementById("medium");
+var hard = document.getElementById("hard");
 var interval = null;
+let num = 1000;
+
+//close window button function
+document.getElementById("close-window").addEventListener("click", () => {
+  window.close();
+});
+
+//onload function
+window.onload = () => {
+  easy.addEventListener("focus", () => {
+    console.log("easy focus in on");
+    num = 1100;
+    console.log(num);
+  });
+  medium.addEventListener("focus", () => {
+    console.log("medium focus in on");
+    num = 740;
+    console.log(num);
+  });
+  hard.addEventListener("focus", () => {
+    num = 290;
+    console.log(num);
+  });
+};
+
+//function to mov the mouse  randomly
+
 startMouse = () => {
   interval = setInterval(() => {
     console.log("mouse is moving");
     Mouse.style.left = Math.floor(Math.random() * 100 + 1) + "%";
     Mouse.style.top = Math.floor(Math.random() * 100 + 1) + "%";
-  }, 1000);
+  }, num);
+
   Mouse.addEventListener("click", GameEnds);
 };
+//function to stopr the mouse
 stopMouse = () => {
-  // cancelled = true;
   console.log("canceleed inter");
   clearInterval(interval);
   gameBody.style.pointerEvents = "none";
@@ -27,7 +56,7 @@ stopMouse = () => {
 GameStart = () => {
   gameBody.style.pointerEvents = "all";
   Mouse.classList.remove("mouse-stop");
-  // Mouse.classList.add("mouse-start");
+
   startMouse();
 
   start();
@@ -36,13 +65,12 @@ Start.addEventListener("click", GameStart);
 
 //Game ends function on clicking the mouse
 GameEnds = () => {
-  // Mouse.classList.add("mouse-stop");
   stopMouse();
   console.log("stop clicked");
   Modal.classList.add("open-modal");
   container.style.opacity = "0.4";
   container.style.pointerEvents = "none";
-  // ModalTime.innerHTML = `${hour} Hours:${minute}Mins:${second}Secs`;
+
   if (hour == 0) {
     ModalTime.innerHTML = `${minute} Minutes:${second} Secs`;
   }
